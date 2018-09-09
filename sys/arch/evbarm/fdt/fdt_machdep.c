89032dcefb2fc55ceaf9ab89282f7da3c2288c2c
Nick Hudson <skrll@netbsd.org>
2018-09-09 21:34:59 +0100

    VPRINTF improvement

--- sys/arch/evbarm/fdt/fdt_machdep.c
+++ sys/arch/evbarm/fdt/fdt_machdep.c
@@ -461,7 +461,7 @@ initarm(void *arg)
 #endif
 	uint64_t memory_size = memory_end - memory_start;
 
-	VPRINTF("%s: memory start %" PRIx64 "end %" PRIx64 "(len %"
+	VPRINTF("%s: memory start %" PRIx64 " end %" PRIx64 " (len %"
 	    PRIx64 ")\n", __func__, memory_start, memory_end, memory_size);
 
 	/* Parse ramdisk info */
