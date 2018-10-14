0bc2397b915d0ae2c94eebe2b31c7c04beaf3758
Nick Hudson <skrll@netbsd.org>
2018-10-14 15:07:18 +0100

    Use __nothing

--- sys/arch/arm/broadcom/bcm283x_platform.c
+++ sys/arch/arm/broadcom/bcm283x_platform.c
@@ -142,7 +142,7 @@ static paddr_t bcm2836_bus_to_phys(bus_addr_t);
 #ifdef VERBOSE_INIT_ARM
 #define VPRINTF(...)	printf(__VA_ARGS__)
 #else
-#define VPRINTF(...)	do { } while (/* CONSTCOND */ 0)
+#define VPRINTF(...)	__nothing
 #endif
 
 static paddr_t
