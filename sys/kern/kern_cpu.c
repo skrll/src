e091b203c376f844a7dd0b5cdb91c96a77833483
Nick Hudson <skrll@netbsd.org>
2018-11-02 09:16:25 +0000

    sched_takecpu can call cpu_lookup with maxcpus as it loops.  Handle this but returning NULL.

--- sys/kern/kern_cpu.c
+++ sys/kern/kern_cpu.c
@@ -297,6 +297,9 @@ cpu_lookup(u_int idx)
 		return curcpu();
 	}
 
+	if (idx == maxcpus)
+		return NULL;
+
 	ci = cpu_infos[idx];
 	KASSERT(ci == NULL || cpu_index(ci) == idx);
 	KASSERTMSG(idx != maxcpus || cpu_infos[idx] == NULL, "idx %d ci %p",
