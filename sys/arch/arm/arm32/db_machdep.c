10cb9cb1b4453ae921db5dfc64d306bb9da084d6
Nick Hudson <skrll@netbsd.org>
2020-07-03 18:16:39 +0100

    Reduce diff to trunk

--- sys/arch/arm/arm32/db_machdep.c
+++ sys/arch/arm/arm32/db_machdep.c
@@ -61,7 +61,6 @@ __KERNEL_RCSID(0, "$NetBSD: db_machdep.c,v 1.34 2020/07/03 10:19:18 jmcneill Exp
 #ifdef _KERNEL
 static long nil;
 
-void db_reset_cmd(db_expr_t, bool, db_expr_t, const char *);
 void db_md_cpuinfo_cmd(db_expr_t, bool, db_expr_t, const char *);
 
 int db_access_und_sp(const struct db_variable *, db_expr_t *, int);
