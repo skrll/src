71ddaa1d28123bab20d94d5da5b5c215b8513e80
Nick Hudson <skrll@netbsd.org>
2020-08-14 08:23:54 +0000

    Don't do kpreemption on this branch

--- sys/arch/evbarm/include/intr.h
+++ sys/arch/evbarm/include/intr.h
@@ -67,10 +67,6 @@
 
 #define IST_MPSAFE	0x100	/* interrupt is MPSAFE */
 
-#if defined(MULTIPROCESSOR) && defined(__HAVE_FAST_SOFTINTS)
-#define	__HAVE_PREEMPTION       1
-#endif
-
 #ifndef _LOCORE
 
 #include <sys/queue.h>
