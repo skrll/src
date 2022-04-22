80568f16bcb659f13392bf5f808225028dd033bd
Nick Hudson <skrll@netbsd.org>
2022-04-22 13:42:55 +0100

    apple: WIP spmi

--- sys/arch/arm/apple/apple_spmi.c
+++ sys/arch/arm/apple/apple_spmi.c
@@ -52,18 +52,7 @@ __KERNEL_RCSID(0, "$NetBSD$");
 
 #include <sys/param.h>
 #include <sys/bus.h>
-//#include <sys/systm.h>
 #include <sys/device.h>
-
-#if 0
-#include <machine/bus.h>
-#include <machine/fdt.h>
-
-#include <dev/ofw/openfirm.h>
-#include <dev/ofw/fdt.h>
-
-#include <dev/fdt/spmivar.h>
-#endif
 
 #include <dev/fdt/fdtvar.h>
 
@@ -75,103 +64,42 @@ __KERNEL_RCSID(0, "$NetBSD$");
  */
 
 #define SPMI_STAT		0x00
-#define  SPMI_STAT_RXEMPTY		(1 << 24)
-#define  SPMI_STAT_TXEMPTY		(1 << 8)
+#define  SPMI_STAT_RXEMPTY		__BIT(24)
+#define  SPMI_STAT_TXEMPTY		__BIT(8)
 #define SPMI_CMD		0x04
-#define  SPMI_CMD_ADDR(x)		((x) << 16)
-#define  SPMI_CMD_LAST			(1 << 15)
-#define  SPMI_CMD_SID(x)		((x) << 8)
+#define  SPMI_CMD_ADDR_MASK		__BITS(23, 16)
+#define  SPMI_CMD_ADDR(x)		__SHIFTIN((x), SPMI_CMD_ADDR_MASK)
+#define  SPMI_CMD_LAST			__BIT(15)
+#define  SPMI_CMD_SID_MASK		__BITS(14, 8)
+#define  SPMI_CMD_SID(x)		__SHIFTIN((x), SPMI_CMD_SID_MASK)
+#define  SPMI_CMD_OPC_MASK		__BITS(7, 0)
 #define SPMI_RESP		0x08
 #define SPMI_INTEN(i)		(0x20 + (i) * 4)
 #define SPMI_INTSTAT(i)		(0x60 + (i) * 4)
 
 #define SPMI_READ(sc, reg)							\
-	(bus_space_read_4((sc)->sc_iot, (sc)->sc_ioh, (reg)))
-#define HWRITE4(sc, reg, val)						\
-	bus_space_write_4((sc)->sc_iot, (sc)->sc_ioh, (reg), (val))
+	(bus_space_read_4((sc)->sc_bst, (sc)->sc_bsh, (reg)))
+#define SPMI_WRITE(sc, reg, val)						\
+	bus_space_write_4((sc)->sc_bst, (sc)->sc_bsh, (reg), (val))
 
-struct aplspmi_softc {
-	struct device		sc_dev;
-	bus_space_tag_t		sc_iot;
-	bus_space_handle_t	sc_ioh;
+struct apple_spmi_softc {
+	device_t		sc_dev;
+	bus_space_tag_t		sc_bst;
+	bus_space_handle_t	sc_bsh;
 
 	struct spmi_controller	sc_tag;
 };
 
-int	aplspmi_match(struct device *, void *, void *);
-void	aplspmi_attach(struct device *, struct device *, void *);
-
-const struct cfattach	aplspmi_ca = {
-	sizeof (struct aplspmi_softc), aplspmi_match, aplspmi_attach
-};
-
-struct cfdriver aplspmi_cd = {
-	NULL, "aplspmi", DV_DULL
-};
-
-int	aplspmi_print(void *, const char *);
-int	aplspmi_cmd_read(void *, uint8_t, uint8_t, uint16_t, void *, size_t);
-int	aplspmi_cmd_write(void *, uint8_t, uint8_t, uint16_t,
+int	apple_spmi_print(void *, const char *);
+int	apple_spmi_cmd_read(void *, uint8_t, uint8_t, uint16_t, void *, size_t);
+int	apple_spmi_cmd_write(void *, uint8_t, uint8_t, uint16_t,
 	    const void *, size_t);
 
-int
-aplspmi_match(struct device *parent, void *match, void *aux)
-{
-	struct fdt_attach_args *faa = aux;
 
-	return OF_is_compatible(faa->fa_node, "apple,spmi");
-}
-
-void
-aplspmi_attach(struct device *parent, struct device *self, void *aux)
-{
-	struct aplspmi_softc *sc = (struct aplspmi_softc *)self;
-	struct fdt_attach_args *faa = aux;
-	struct spmi_attach_args sa;
-	char name[32];
-	uint32_t reg[2];
-	int node;
-
-	if (faa->fa_nreg < 1) {
-		printf(": no registers\n");
-		return;
-	}
-
-	sc->sc_iot = faa->fa_iot;
-	if (bus_space_map(sc->sc_iot, faa->fa_reg[0].addr,
-	    faa->fa_reg[0].size, 0, &sc->sc_ioh)) {
-		printf(": can't map registers\n");
-		return;
-	}
-
-	printf("\n");
-
-	sc->sc_tag.sc_cookie = sc;
-	sc->sc_tag.sc_cmd_read = aplspmi_cmd_read;
-	sc->sc_tag.sc_cmd_write = aplspmi_cmd_write;
-
-	for (node = OF_child(faa->fa_node); node; node = OF_peer(node)) {
-		if (OF_getpropintarray(node, "reg", reg,
-		    sizeof(reg)) != sizeof(reg))
-			continue;
-
-		memset(name, 0, sizeof(name));
-		if (OF_getprop(node, "compatible", name, sizeof(name)) == -1)
-			continue;
-		if (name[0] == '\0')
-			continue;
-
-		memset(&sa, 0, sizeof(sa));
-		sa.sa_tag = &sc->sc_tag;
-		sa.sa_sid = reg[0];
-		sa.sa_name = name;
-		sa.sa_node = node;
-		config_found(self, &sa, aplspmi_print);
-	}
-}
+#if 0
 
 int
-aplspmi_print(void *aux, const char *pnp)
+apple_spmi_print(void *aux, const char *pnp)
 {
 	struct spmi_attach_args *sa = aux;
 
@@ -181,9 +109,11 @@ aplspmi_print(void *aux, const char *pnp)
 
 	return UNCONF;
 }
+#endif
+
 
 int
-aplspmi_read_resp(struct aplspmi_softc *sc, uint32_t *resp)
+apple_spmi_read_resp(struct apple_spmi_softc *sc, uint32_t *resp)
 {
 	int retry;
 
@@ -200,10 +130,10 @@ aplspmi_read_resp(struct aplspmi_softc *sc, uint32_t *resp)
 }
 
 int
-aplspmi_cmd_read(void *cookie, uint8_t sid, uint8_t cmd, uint16_t addr,
+apple_spmi_cmd_read(void *cookie, uint8_t sid, uint8_t cmd, uint16_t addr,
     void *buf, size_t len)
 {
-	struct aplspmi_softc *sc = cookie;
+	struct apple_spmi_softc *sc = cookie;
 	uint8_t *cbuf = buf;
 	uint32_t resp;
 	int error;
@@ -211,15 +141,15 @@ aplspmi_cmd_read(void *cookie, uint8_t sid, uint8_t cmd, uint16_t addr,
 	if (len == 0 || len > 8)
 		return EINVAL;
 
-	HWRITE4(sc, SPMI_CMD, SPMI_CMD_SID(sid) | cmd | SPMI_CMD_ADDR(addr) |
+	SPMI_WRITE(sc, SPMI_CMD, SPMI_CMD_SID(sid) | cmd | SPMI_CMD_ADDR(addr) |
 	    (len - 1) | SPMI_CMD_LAST);
 
-	error = aplspmi_read_resp(sc, &resp);
+	error = apple_spmi_read_resp(sc, &resp);
 	if (error)
 		return error;
 
 	while (len > 0) {
-		error = aplspmi_read_resp(sc, &resp);
+		error = apple_spmi_read_resp(sc, &resp);
 		if (error)
 			return error;
 		memcpy(cbuf, &resp, MIN(len, 4));
@@ -231,25 +161,117 @@ aplspmi_cmd_read(void *cookie, uint8_t sid, uint8_t cmd, uint16_t addr,
 }
 
 int
-aplspmi_cmd_write(void *cookie, uint8_t sid, uint8_t cmd, uint16_t addr,
+apple_spmi_cmd_write(void *cookie, uint8_t sid, uint8_t cmd, uint16_t addr,
     const void *buf, size_t len)
 {
-	struct aplspmi_softc *sc = cookie;
+	struct apple_spmi_softc *sc = cookie;
 	const uint8_t *cbuf = buf;
 	uint32_t data, resp;
 
 	if (len == 0 || len > 8)
 		return EINVAL;
 
-	HWRITE4(sc, SPMI_CMD, SPMI_CMD_SID(sid) | cmd | SPMI_CMD_ADDR(addr) |
+	SPMI_WRITE(sc, SPMI_CMD, SPMI_CMD_SID(sid) | cmd | SPMI_CMD_ADDR(addr) |
 	    (len - 1) | SPMI_CMD_LAST);
 
 	while (len > 0) {
 		memcpy(&data, cbuf, MIN(len, 4));
-		HWRITE4(sc, SPMI_CMD, data);
+		SPMI_WRITE(sc, SPMI_CMD, data);
 		cbuf += MIN(len, 4);
 		len -= MIN(len, 4);
 	}
 
-	return aplspmi_read_resp(sc, &resp);
+	return apple_spmi_read_resp(sc, &resp);
 }
+
+
+
+static const struct device_compatible_entry compat_data[] = {
+	{ .compat = "apple,spmi" },
+	DEVICE_COMPAT_EOL
+};
+
+static int
+apple_iic_match(device_t parent, cfdata_t cf, void *aux)
+{
+	struct fdt_attach_args * const faa = aux;
+
+	return of_compatible_match(faa->faa_phandle, compat_data);
+}
+
+static void
+apple_iic_attach(device_t parent, device_t self, void *aux)
+{
+	struct apple_iic_softc * const sc = device_private(self);
+	struct fdt_attach_args * const faa = aux;
+	const int phandle = faa->faa_phandle;
+
+
+void
+apple_spmi_attach(device_t *parent, device_t *self, void *aux)
+{
+	struct apple_spmi_softc *sc = (struct apple_spmi_softc *)self;
+	struct fdt_attach_args *faa = aux;
+	struct spmi_attach_args sa;
+	char name[32];
+	uint32_t reg[2];
+	int node;
+
+	bus_addr_t addr;
+	bus_size_t size;
+
+	sc->sc_dev = self;
+	sc->sc_bst = faa->faa_bst;
+
+	int error = fdtbus_get_reg(phandle, 0, &addr, &size);
+	if (error) {
+		aprint_error(": unable to get device registers\n");
+		return;
+	}
+
+	if (bus_space_map(sc->sc_bst, addr, size, 0, &sc->sc_bsh)) {
+		aprint_error(": unable to map device\n");
+		return;
+	}
+
+	aprint_naive("\n");
+	aprint_normal(": Apple SPMI\n");
+
+
+
+	sc->sc_tag.sc_cookie = sc;
+	sc->sc_tag.sc_cmd_read = apple_spmi_cmd_read;
+	sc->sc_tag.sc_cmd_write = apple_spmi_cmd_write;
+
+
+	fdtbus_register_spmi_controller(&sc->sc_spmi, phandle);
+
+	fdtbus_attach_spmibus(self, phandle, &sc->sc_spmi, spmibus_print);
+
+#if 0
+	for (node = OF_child(faa->fa_node); node; node = OF_peer(node)) {
+		if (OF_getpropintarray(node, "reg", reg,
+		    sizeof(reg)) != sizeof(reg))
+			continue;
+
+		memset(name, 0, sizeof(name));
+		if (OF_getprop(node, "compatible", name, sizeof(name)) == -1)
+			continue;
+		if (name[0] == '\0')
+			continue;
+
+		memset(&sa, 0, sizeof(sa));
+		sa.sa_tag = &sc->sc_tag;
+		sa.sa_sid = reg[0];
+		sa.sa_name = name;
+		sa.sa_node = node;
+		config_found(self, &sa, apple_spmi_print);
+	}
+#endif
+
+}
+
+
+
+CFATTACH_DECL_NEW(apple_spmi, sizeof(struct apple_spmi_softc),
+    apple_spmi_match, apple_spmi_attach, NULL, NULL);
