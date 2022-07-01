7aec781133e9bc769bca11135fc2c948da363dea
Nick Hudson <skrll@netbsd.org>
2022-07-01 09:57:54 +0100

    Revert "Increase SPINLOCK_SPINOUT"
    This reverts commit c7ec41b1db92a7bf2582b5868ba836c57c56f46a.

--- sys/sys/lock.h
+++ sys/sys/lock.h
@@ -72,7 +72,7 @@ do {								\
 } while (0)
 
 #ifdef LOCKDEBUG
-#define	SPINLOCK_SPINOUT(spins)		((spins)++ > 0x4fffffff)
+#define	SPINLOCK_SPINOUT(spins)		((spins)++ > 0x0fffffff)
 #else
 #define	SPINLOCK_SPINOUT(spins)		((void)(spins), 0)
 #endif
