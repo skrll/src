e6ff360dd65f9efcfd348c2da82ddd1367113a78
Nick Hudson <skrll@netbsd.org>
2024-10-27 07:09:28 +0000

    risc-v: JH7110 pin control

--- sys/arch/riscv/starfive/jh7110_pinctrl.c
+++ sys/arch/riscv/starfive/jh7110_pinctrl.c
@@ -40,8 +40,10 @@ __KERNEL_RCSID(0, "$NetBSD: jh7110_pinctrl.c,v 1.1 2024/11/11 19:23:18 skrll Exp
 
 struct jh7110_pinctrl_softc;
 struct jh7110_pinctrl_data {
+//	const struct pinctrl_pin_desc *pins;
 	u_int		jpd_npins;
 	u_int		jpd_ngpios;
+//	u_int gcsc_base;
 
 	bus_size_t	jpd_dout;
 	uint32_t	jpd_dout_mask;
@@ -50,7 +52,21 @@ struct jh7110_pinctrl_data {
 	bus_size_t	jpd_gpi;
 	uint32_t	jpd_gpi_mask;
 	bus_size_t	jpd_gin;
+
 	bus_size_t	jpd_gpioin;
+
+//	const struct jh7110_gpio_irq_reg *irq_reg;
+
+//	u_int nsaved_regs;
+
+#if 0
+	/* generic pinmux */
+	/* gpio chip */
+	int (*jh7110_get_padcfgsc_base)(struct jh7110_pinctrl *sfp,
+				      u_int pin);
+	void (*jh7110_gpio_irq_handler)(struct irq_desc *desc);
+	int (*jh7110_gpio_init_hw)(struct gpio_chip *gc);
+#endif
 };
 
 struct jh7110_pinctrl_softc {
@@ -83,6 +99,8 @@ struct jh7110_pinctrl_gpio_pin {
 #define JH7110_SYS_NGPIO		64
 #define JH7110_SYS_NPIN			96
 
+//#define JH7110_SYS_GC_BASE		0
+
 /* AON registers */
 #define JH7110_AON_DOEN			0x0000
 #define JH7110_AON_DOUT			0x0004
@@ -91,6 +109,9 @@ struct jh7110_pinctrl_gpio_pin {
 
 #define JH7110_AON_NGPIO		4
 #define JH7110_AON_NPIN			20
+
+//#define JH7110_AON_GC_BASE		64		// XXXNH not needed atm
+//#define JH7110_AON_REGS_NUM		37		// XXXNH not needed atm
 
 // XXXNH rename
 #define GPOUT_LOW			0
@@ -117,6 +138,18 @@ struct jh7110_pinctrl_gpio_pin {
 #define JH7110_PADCFG_SLEW	__BIT(5)
 #define JH7110_PADCFG_SMT	__BIT(6)
 #define JH7110_PADCFG_POS	__BIT(7)
+
+#if 0
+struct jh7110_gpio_irq_reg {
+	u_int is_reg_base;
+	u_int ic_reg_base;
+	u_int ibe_reg_base;
+	u_int iev_reg_base;
+	u_int ie_reg_base;
+	u_int ris_reg_base;
+	u_int mis_reg_base;
+};
+#endif
 
 /* SYS pins */
 #define JH7110_SYS_PAD_GPIO0               0
@@ -214,6 +247,160 @@ struct jh7110_pinctrl_gpio_pin {
 #define JH7110_SYS_PAD_QSPI_DATA1          92
 #define JH7110_SYS_PAD_QSPI_DATA2          93
 #define JH7110_SYS_PAD_QSPI_DATA3          94
+
+
+/* AON pins */
+#define JH7110_AON_PAD_TESTEN              0
+#define JH7110_AON_PAD_RGPIO0              1
+#define JH7110_AON_PAD_RGPIO1              2
+#define JH7110_AON_PAD_RGPIO2              3
+#define JH7110_AON_PAD_RGPIO3              4
+#define JH7110_AON_PAD_RSTN                5
+// doesn't match the register layout in AON IOMUX CFG SAIF SYSCFG'
+#define JH7110_AON_PAD_GMAC0_MDC           6
+#define JH7110_AON_PAD_GMAC0_MDIO          7
+#define JH7110_AON_PAD_GMAC0_RXD0          8
+#define JH7110_AON_PAD_GMAC0_RXD1          9
+#define JH7110_AON_PAD_GMAC0_RXD2          10
+#define JH7110_AON_PAD_GMAC0_RXD3          11
+#define JH7110_AON_PAD_GMAC0_RXDV          12
+#define JH7110_AON_PAD_GMAC0_RXC           13
+#define JH7110_AON_PAD_GMAC0_TXD0          14
+#define JH7110_AON_PAD_GMAC0_TXD1          15
+#define JH7110_AON_PAD_GMAC0_TXD2          16
+#define JH7110_AON_PAD_GMAC0_TXD3          17
+#define JH7110_AON_PAD_GMAC0_TXEN          18
+#define JH7110_AON_PAD_GMAC0_TXC           19
+
+
+#if 0
+// https://doc-en.rvspace.org/JH7110/TRM/JH7110_TRM/sys_iomux_cfg.html#sys_iomux_cfg__section_gtq_t3b_xsb
+
+static const struct pinctrl_pin_desc jh7110_sys_pins[] = {
+	PINCTRL_PIN(PAD_GPIO0,		"GPIO0"),
+	PINCTRL_PIN(PAD_GPIO1,		"GPIO1"),
+	PINCTRL_PIN(PAD_GPIO2,		"GPIO2"),
+	PINCTRL_PIN(PAD_GPIO3,		"GPIO3"),
+	PINCTRL_PIN(PAD_GPIO4,		"GPIO4"),
+	PINCTRL_PIN(PAD_GPIO5,		"GPIO5"),
+	PINCTRL_PIN(PAD_GPIO6,		"GPIO6"),
+	PINCTRL_PIN(PAD_GPIO7,		"GPIO7"),
+	PINCTRL_PIN(PAD_GPIO8,		"GPIO8"),
+	PINCTRL_PIN(PAD_GPIO9,		"GPIO9"),
+	PINCTRL_PIN(PAD_GPIO10,		"GPIO10"),
+	PINCTRL_PIN(PAD_GPIO11,		"GPIO11"),
+	PINCTRL_PIN(PAD_GPIO12,		"GPIO12"),
+	PINCTRL_PIN(PAD_GPIO13,		"GPIO13"),
+	PINCTRL_PIN(PAD_GPIO14,		"GPIO14"),
+	PINCTRL_PIN(PAD_GPIO15,		"GPIO15"),
+	PINCTRL_PIN(PAD_GPIO16,		"GPIO16"),
+	PINCTRL_PIN(PAD_GPIO17,		"GPIO17"),
+	PINCTRL_PIN(PAD_GPIO18,		"GPIO18"),
+	PINCTRL_PIN(PAD_GPIO19,		"GPIO19"),
+	PINCTRL_PIN(PAD_GPIO20,		"GPIO20"),
+	PINCTRL_PIN(PAD_GPIO21,		"GPIO21"),
+	PINCTRL_PIN(PAD_GPIO22,		"GPIO22"),
+	PINCTRL_PIN(PAD_GPIO23,		"GPIO23"),
+	PINCTRL_PIN(PAD_GPIO24,		"GPIO24"),
+	PINCTRL_PIN(PAD_GPIO25,		"GPIO25"),
+	PINCTRL_PIN(PAD_GPIO26,		"GPIO26"),
+	PINCTRL_PIN(PAD_GPIO27,		"GPIO27"),
+	PINCTRL_PIN(PAD_GPIO28,		"GPIO28"),
+	PINCTRL_PIN(PAD_GPIO29,		"GPIO29"),
+	PINCTRL_PIN(PAD_GPIO30,		"GPIO30"),
+	PINCTRL_PIN(PAD_GPIO31,		"GPIO31"),
+	PINCTRL_PIN(PAD_GPIO32,		"GPIO32"),
+	PINCTRL_PIN(PAD_GPIO33,		"GPIO33"),
+	PINCTRL_PIN(PAD_GPIO34,		"GPIO34"),
+	PINCTRL_PIN(PAD_GPIO35,		"GPIO35"),
+	PINCTRL_PIN(PAD_GPIO36,		"GPIO36"),
+	PINCTRL_PIN(PAD_GPIO37,		"GPIO37"),
+	PINCTRL_PIN(PAD_GPIO38,		"GPIO38"),
+	PINCTRL_PIN(PAD_GPIO39,		"GPIO39"),
+	PINCTRL_PIN(PAD_GPIO40,		"GPIO40"),
+	PINCTRL_PIN(PAD_GPIO41,		"GPIO41"),
+	PINCTRL_PIN(PAD_GPIO42,		"GPIO42"),
+	PINCTRL_PIN(PAD_GPIO43,		"GPIO43"),
+	PINCTRL_PIN(PAD_GPIO44,		"GPIO44"),
+	PINCTRL_PIN(PAD_GPIO45,		"GPIO45"),
+	PINCTRL_PIN(PAD_GPIO46,		"GPIO46"),
+	PINCTRL_PIN(PAD_GPIO47,		"GPIO47"),
+	PINCTRL_PIN(PAD_GPIO48,		"GPIO48"),
+	PINCTRL_PIN(PAD_GPIO49,		"GPIO49"),
+	PINCTRL_PIN(PAD_GPIO50,		"GPIO50"),
+	PINCTRL_PIN(PAD_GPIO51,		"GPIO51"),
+	PINCTRL_PIN(PAD_GPIO52,		"GPIO52"),
+	PINCTRL_PIN(PAD_GPIO53,		"GPIO53"),
+	PINCTRL_PIN(PAD_GPIO54,		"GPIO54"),
+	PINCTRL_PIN(PAD_GPIO55,		"GPIO55"),
+	PINCTRL_PIN(PAD_GPIO56,		"GPIO56"),
+	PINCTRL_PIN(PAD_GPIO57,		"GPIO57"),
+	PINCTRL_PIN(PAD_GPIO58,		"GPIO58"),
+	PINCTRL_PIN(PAD_GPIO59,		"GPIO59"),
+	PINCTRL_PIN(PAD_GPIO60,		"GPIO60"),
+	PINCTRL_PIN(PAD_GPIO61,		"GPIO61"),
+	PINCTRL_PIN(PAD_GPIO62,		"GPIO62"),
+	PINCTRL_PIN(PAD_GPIO63,		"GPIO63"),
+	PINCTRL_PIN(PAD_SD0_CLK,	"SD0_CLK"),
+	PINCTRL_PIN(PAD_SD0_CMD,	"SD0_CMD"),
+	PINCTRL_PIN(PAD_SD0_DATA0,	"SD0_DATA0"),
+	PINCTRL_PIN(PAD_SD0_DATA1,	"SD0_DATA1"),
+	PINCTRL_PIN(PAD_SD0_DATA2,	"SD0_DATA2"),
+	PINCTRL_PIN(PAD_SD0_DATA3,	"SD0_DATA3"),
+	PINCTRL_PIN(PAD_SD0_DATA4,	"SD0_DATA4"),
+	PINCTRL_PIN(PAD_SD0_DATA5,	"SD0_DATA5"),
+	PINCTRL_PIN(PAD_SD0_DATA6,	"SD0_DATA6"),
+	PINCTRL_PIN(PAD_SD0_DATA7,	"SD0_DATA7"),
+	PINCTRL_PIN(PAD_SD0_STRB,	"SD0_STRB"),
+	PINCTRL_PIN(PAD_GMAC1_MDC,	"GMAC1_MDC"),
+	PINCTRL_PIN(PAD_GMAC1_MDIO,	"GMAC1_MDIO"),
+	PINCTRL_PIN(PAD_GMAC1_RXD0,	"GMAC1_RXD0"),
+	PINCTRL_PIN(PAD_GMAC1_RXD1,	"GMAC1_RXD1"),
+	PINCTRL_PIN(PAD_GMAC1_RXD2,	"GMAC1_RXD2"),
+	PINCTRL_PIN(PAD_GMAC1_RXD3,	"GMAC1_RXD3"),
+	PINCTRL_PIN(PAD_GMAC1_RXDV,	"GMAC1_RXDV"),
+	PINCTRL_PIN(PAD_GMAC1_RXC,	"GMAC1_RXC"),
+	PINCTRL_PIN(PAD_GMAC1_TXD0,	"GMAC1_TXD0"),
+	PINCTRL_PIN(PAD_GMAC1_TXD1,	"GMAC1_TXD1"),
+	PINCTRL_PIN(PAD_GMAC1_TXD2,	"GMAC1_TXD2"),
+	PINCTRL_PIN(PAD_GMAC1_TXD3,	"GMAC1_TXD3"),
+	PINCTRL_PIN(PAD_GMAC1_TXEN,	"GMAC1_TXEN"),
+	PINCTRL_PIN(PAD_GMAC1_TXC,	"GMAC1_TXC"),
+	PINCTRL_PIN(PAD_QSPI_SCLK,	"QSPI_SCLK"),
+	PINCTRL_PIN(PAD_QSPI_CS0,	"QSPI_CS0"),
+	PINCTRL_PIN(PAD_QSPI_DATA0,	"QSPI_DATA0"),
+	PINCTRL_PIN(PAD_QSPI_DATA1,	"QSPI_DATA1"),
+	PINCTRL_PIN(PAD_QSPI_DATA2,	"QSPI_DATA2"),
+	PINCTRL_PIN(PAD_QSPI_DATA3,	"QSPI_DATA3"),
+};
+
+
+// https://doc-en.rvspace.org/JH7110/TRM/JH7110_TRM/aon_iomux_cfg.html#aon_iomux_cfg__section_xl4_w3b_xsb
+static const struct pinctrl_pin_desc jh7110_aon_pins[] = {
+	PINCTRL_PIN(PAD_TESTEN,		"TESTEN"),
+	PINCTRL_PIN(PAD_RGPIO0,		"RGPIO0"),
+	PINCTRL_PIN(PAD_RGPIO1,		"RGPIO1"),
+	PINCTRL_PIN(PAD_RGPIO2,		"RGPIO2"),
+	PINCTRL_PIN(PAD_RGPIO3,		"RGPIO3"),
+	PINCTRL_PIN(PAD_RSTN,		"RSTN"),
+// RTC_D
+	PINCTRL_PIN(PAD_GMAC0_MDC,	"GMAC0_MDC"),
+	PINCTRL_PIN(PAD_GMAC0_MDIO,	"GMAC0_MDIO"),
+	PINCTRL_PIN(PAD_GMAC0_RXD0,	"GMAC0_RXD0"),
+	PINCTRL_PIN(PAD_GMAC0_RXD1,	"GMAC0_RXD1"),
+	PINCTRL_PIN(PAD_GMAC0_RXD2,	"GMAC0_RXD2"),
+	PINCTRL_PIN(PAD_GMAC0_RXD3,	"GMAC0_RXD3"),
+	PINCTRL_PIN(PAD_GMAC0_RXDV,	"GMAC0_RXDV"),
+	PINCTRL_PIN(PAD_GMAC0_RXC,	"GMAC0_RXC"),
+	PINCTRL_PIN(PAD_GMAC0_TXD0,	"GMAC0_TXD0"),
+	PINCTRL_PIN(PAD_GMAC0_TXD1,	"GMAC0_TXD1"),
+	PINCTRL_PIN(PAD_GMAC0_TXD2,	"GMAC0_TXD2"),
+	PINCTRL_PIN(PAD_GMAC0_TXD3,	"GMAC0_TXD3"),
+	PINCTRL_PIN(PAD_GMAC0_TXEN,	"GMAC0_TXEN"),
+	PINCTRL_PIN(PAD_GMAC0_TXC,	"GMAC0_TXC"),
+};
+
+#endif
 
 struct jh7110_func_sel {
     uint16_t	jfs_funcreg;
@@ -323,6 +510,91 @@ jh7110_set_function(struct jh7110_pinctrl_softc *sc, u_int pin_no,
 	mutex_exit(&sc->sc_lock);
 }
 
+#if 0
+//
+static const struct jh7110_vin_group_sel
+	jh7110_sys_vin_group_sel[ARRAY_SIZE(jh7110_sys_pins)] = {
+	[PAD_GPIO6]     = { 0x2b4, 21, 0 },
+	[PAD_GPIO7]     = { 0x2b4, 18, 0 },
+	[PAD_GPIO8]     = { 0x2b4, 15, 0 },
+	[PAD_GPIO9]     = { 0x2b0, 11, 0 },
+	[PAD_GPIO10]    = { 0x2b0, 20, 0 },
+	[PAD_GPIO11]    = { 0x2b0, 23, 0 },
+	[PAD_GPIO12]    = { 0x2b0, 26, 0 },
+	[PAD_GPIO13]    = { 0x2b0, 29, 0 },
+	[PAD_GPIO14]    = { 0x2b4,  0, 0 },
+	[PAD_GPIO15]    = { 0x2b4,  3, 0 },
+	[PAD_GPIO16]    = { 0x2b4,  6, 0 },
+	[PAD_GPIO17]    = { 0x2b4,  9, 0 },
+	[PAD_GPIO18]    = { 0x2b4, 12, 0 },
+	[PAD_GPIO19]    = { 0x2b0, 14, 0 },
+	[PAD_GPIO20]    = { 0x2b0, 17, 0 },
+
+	[PAD_GPIO21]    = { 0x2b4, 21, 1 },
+	[PAD_GPIO22]    = { 0x2b4, 18, 1 },
+	[PAD_GPIO23]    = { 0x2b4, 15, 1 },
+	[PAD_GPIO24]    = { 0x2b0, 11, 1 },
+	[PAD_GPIO25]    = { 0x2b0, 20, 1 },
+	[PAD_GPIO26]    = { 0x2b0, 23, 1 },
+	[PAD_GPIO27]    = { 0x2b0, 26, 1 },
+	[PAD_GPIO28]    = { 0x2b0, 29, 1 },
+	[PAD_GPIO29]    = { 0x2b4,  0, 1 },
+	[PAD_GPIO30]    = { 0x2b4,  3, 1 },
+	[PAD_GPIO31]    = { 0x2b4,  6, 1 },
+	[PAD_GPIO32]    = { 0x2b4,  9, 1 },
+	[PAD_GPIO33]    = { 0x2b4, 12, 1 },
+	[PAD_GPIO34]    = { 0x2b0, 14, 1 },
+	[PAD_GPIO35]    = { 0x2b0, 17, 1 },
+
+	[PAD_GPIO36]    = { 0x2b4, 21, 2 },
+	[PAD_GPIO37]    = { 0x2b4, 18, 2 },
+	[PAD_GPIO38]    = { 0x2b4, 15, 2 },
+	[PAD_GPIO39]    = { 0x2b0, 11, 2 },
+	[PAD_GPIO40]    = { 0x2b0, 20, 2 },
+	[PAD_GPIO41]    = { 0x2b0, 23, 2 },
+	[PAD_GPIO42]    = { 0x2b0, 26, 2 },
+	[PAD_GPIO43]    = { 0x2b0, 29, 2 },
+	[PAD_GPIO44]    = { 0x2b4,  0, 2 },
+	[PAD_GPIO45]    = { 0x2b4,  3, 2 },
+	[PAD_GPIO46]    = { 0x2b4,  6, 2 },
+	[PAD_GPIO47]    = { 0x2b4,  9, 2 },
+	[PAD_GPIO48]    = { 0x2b4, 12, 2 },
+	[PAD_GPIO49]    = { 0x2b0, 14, 2 },
+	[PAD_GPIO50]    = { 0x2b0, 17, 2 },
+};
+
+
+static void
+jh7110_set_vin_group(struct jh7110_pinctrl_softc *sc, u_int pin_no)
+{
+	if (pin_no >= __arraycount(jh7110_sys_func_sel))
+		return;
+
+	const struct jh7110_func_sel * const jfs =
+	    &jh7110_sys_func_sel[pin_no];
+
+	const struct jh7110_vin_group_sel *gs = &jh7110_sys_vin_group_sel[pin];
+	unsigned long flags;
+	void __iomem *reg;
+	u32 mask;
+	u32 grp;
+
+	if (!gs->offset)
+		return;
+
+	reg = sfp->base + gs->offset;
+	grp = gs->group << gs->shift;
+	mask = 0x3U << gs->shift;
+
+	raw_spin_lock_irqsave(&sfp->lock, flags);
+	grp |= readl_relaxed(reg) & ~mask;
+	writel_relaxed(grp, reg);
+	raw_spin_unlock_irqrestore(&sfp->lock, flags);
+}
+
+
+#endif
+
 
 static void
 jh7110_set_gpiomux(struct jh7110_pinctrl_softc * const sc, u_int pin_no,
@@ -367,12 +639,23 @@ jh7110_set_gpiomux(struct jh7110_pinctrl_softc * const sc, u_int pin_no,
 		WR4(sc, din_reg, dinval);
 	}
 	mutex_exit(&sc->sc_lock);
+
+	aprint_debug_dev(sc->sc_dev, "set_config: "
+	    "gpio %d dout %#x/%#x doen %#x/%#x din %#x/%#x\n",
+	    pin_no,
+	    doutval, doutold,
+	    doenval, doenold,
+	    din != GPI_NONE ? dinval : 0,
+	    din != GPI_NONE ? dinold : 0);
 }
 
 
 static const struct jh7110_pinctrl_data jh7110_aon_pinctrl_data = {
+//	.jpd_pins	= jh7110_aon_pins,
+//	.jpd_npins	= ARRAY_SIZE(jh7110_aon_pins),
 	.jpd_npins	= JH7110_AON_NPIN,
 	.jpd_ngpios	= JH7110_AON_NGPIO,
+//	.gc	= JH7110_AON_GC_BASE,
 	.jpd_doen	= JH7110_AON_DOEN,
 	.jpd_doen_mask	= __BITS(2, 0),
 	.jpd_dout	= JH7110_AON_DOUT,
@@ -380,11 +663,23 @@ static const struct jh7110_pinctrl_data jh7110_aon_pinctrl_data = {
 	.jpd_gpi	= JH7110_AON_GPI,
 	.jpd_gpi_mask	= __BITS(3, 0),
 	.jpd_gpioin	= JH7110_AON_GPIOIN,
+
+//	.irq_reg		   = &jh7110_aon_irq_reg,
+//	.nsaved_regs		   = JH7110_AON_REGS_NUM,
+//	.jh7110_get_padcfgsc_base  = jh7110_aon_get_padcfgsc_base,
+//	.jh7110_gpio_irq_handler = jh7110_aon_irq_handler,
+//	.jh7110_gpio_init_hw	 = jh7110_aon_init_hw,
 };
 
+
+
 static const struct jh7110_pinctrl_data jh7110_sys_pinctrl_data = {
+//	.pins		= jh7110_sys_pins,
+//	.npins		= ARRAY_SIZE(jh7110_sys_pins),
 	.jpd_npins	= JH7110_SYS_NPIN,
 	.jpd_ngpios	= JH7110_SYS_NGPIO,
+//	.jpd_gc	= JH7110_SYS_GC_BASE,
+
 	.jpd_doen	= JH7110_SYS_DOEN,
 	.jpd_doen_mask	= __BITS(5, 0),
 	.jpd_dout	= JH7110_SYS_DOUT,
@@ -392,6 +687,13 @@ static const struct jh7110_pinctrl_data jh7110_sys_pinctrl_data = {
 	.jpd_gpi	= JH7110_SYS_GPI,
 	.jpd_gpi_mask	= __BITS(6, 0),
 	.jpd_gpioin	= JH7110_SYS_GPIOIN,
+
+//	.irq		   = &jh7110_sys_irq_reg,
+//	.nsaved_regs		   = JH7110_SYS_REGS_NUM,
+//	.jh7110_set_one_pin_mux  = jh7110_sys_set_one_pin_mux,
+//	.jh7110_get_padcfgsc_base  = jh7110_sys_get_padcfg_base,
+//	.jh7110_gpio_irq_handler = jh7110_sys_irq_handler,
+//	.jh7110_gpio_init_hw	 = jh7110_sys_init_hw,
 };
 
 
@@ -411,9 +713,12 @@ jh7110_set_pinmux(struct jh7110_pinctrl_softc *sc, u_int pin,
 	if (pin < jpd->jpd_npins)
 		jh7110_set_function(sc, pin, func);
 
+#if 0
+	if (pin < jpd->jpd_ngpios && func == 2)
+		jh7110_set_vin_group(sc, pin);
+#endif
 	return 0;
 }
-
 
 /* Device Tree encoding */
 #define DT_PINMUX_DIN_MASK	__BITS(31, 24)
@@ -526,16 +831,27 @@ jh7110_pinctrl_pin_properties(struct jh7110_pinctrl_softc *sc, int phandle,
 static void
 jh7110_pinctrl_set_config_group(struct jh7110_pinctrl_softc *sc, int group)
 {
-	int pinmux_len;
+	int pins_len, pinmux_len;
+	const u_int *pins = fdtbus_get_prop(group, "pins", &pins_len);
 	const u_int *pinmux = fdtbus_get_prop(group, "pinmux", &pinmux_len);
 	size_t plen;
 	const u_int *parray;
 
 	aprint_debug_dev(sc->sc_dev, "set_config: group   %d\n", group);
-	if (pinmux == NULL) {
+	// XXXNH check binding to see if 'pins' makes sense.
+	if (pins == NULL && pinmux == NULL) {
 		aprint_debug_dev(sc->sc_dev, "group %d neither 'pins' nor "
 		    "'pinmux' exist\n", group);
 		return;
+	} else if (pins != NULL && pinmux != NULL) {
+		aprint_debug_dev(sc->sc_dev, "group %d both 'pins' and "
+		    "'pinmux' exist\n", group);
+		return;
+	}
+
+	if (pins != NULL) {
+		plen = pins_len;
+		parray = pins;
 	}
 	if (pinmux != NULL) {
 		plen = pinmux_len;
@@ -632,7 +948,6 @@ jh7110_pinctrl_gpio_release(device_t dev, void *priv)
 	kmem_free(pin, sizeof(*pin));
 }
 
-
 static int
 jh7110_pinctrl_gpio_read(device_t dev, void *priv, bool raw)
 {
@@ -645,6 +960,12 @@ jh7110_pinctrl_gpio_read(device_t dev, void *priv, bool raw)
 	const bus_size_t offset = ((pin_no) / pins_per_bank) * banksz;
 	const uint32_t mask = __BIT(pin_no % pins_per_bank);
 	const uint32_t bank = RD4(sc, sc->sc_jpd->jpd_gpioin + offset);
+
+	aprint_verbose_dev(sc->sc_dev, "pin_no %3d banksz %2zu "
+	    "offset %#5" PRIxBUSSIZE " mask %2x gpioinreg %#"PRIxBUSSIZE" "
+	    "bank %#"PRIx32"\n",
+	    pin_no, banksz, offset, mask,
+	    sc->sc_jpd->jpd_gpioin + offset, bank);
 
 	int val = __SHIFTOUT(bank, mask);
 	if (!raw && pin->pin_actlo)
@@ -674,10 +995,18 @@ jh7110_pinctrl_gpio_write(device_t dev, void *priv, int val, bool raw)
 
 	mutex_enter(&sc->sc_lock);
 	uint32_t bank = RD4(sc, sc->sc_jpd->jpd_dout + offset);
+	uint32_t obank = bank;
 	bank &= ~mask;
 	bank |= __SHIFTIN(val != 0 ? GPOUT_HIGH : GPOUT_LOW, mask);
 	WR4(sc, sc->sc_jpd->jpd_dout + offset, bank);
 	mutex_exit(&sc->sc_lock);
+
+	aprint_verbose_dev(sc->sc_dev, "pin_no %3d banksz %2zu bpb %2u "
+	    "bpp %2u offset %#5" PRIxBUSSIZE " mask %2x "
+	    "doutreg %#"PRIxBUSSIZE" bank %#"PRIx32"/%#"PRIx32"\n",
+	    pin_no, banksz, bits_per_bank, bits_per_pin, offset, mask,
+	    sc->sc_jpd->jpd_dout_mask + offset, obank, bank);
+
 }
 
 
@@ -717,6 +1046,12 @@ jh7110_pinctrl_attach(device_t parent, device_t self, void *aux)
 	sc->sc_dev = self;
 	sc->sc_phandle = phandle;
 	sc->sc_bst = faa->faa_bst;
+#if 0
+	if (!of_hasprop(phandle, "gpio-controller")) {
+		aprint_error(": no gpio controller");
+		return;
+	}
+#endif
 	error = fdtbus_get_reg(phandle, 0, &addr, &size);
 	if (error) {
 		aprint_error(": couldn't get registers\n");
@@ -730,6 +1065,28 @@ jh7110_pinctrl_attach(device_t parent, device_t self, void *aux)
 		return;
 	}
 
+	// check for pins / npins
+#if 0
+	/* enable clocks */
+	struct clk *clk;
+	fdtbus_clock_assign(phandle);
+	for (u_int c = 0; (clk = fdtbus_clock_get_index(phandle, c)) != NULL; c++) {
+		if (clk_enable(clk) != 0) {
+			aprint_error(": couldn't enable clock #%d\n", c);
+			// XXXNH cleanup
+			return;
+		}
+	}
+	/* de-assert resets */
+	struct fdtbus_reset *rst;
+	for (u_int r = 0; (rst = fdtbus_reset_get_index(phandle, r)) != NULL; r++) {
+		if (fdtbus_reset_deassert(rst) != 0) {
+			aprint_error(": couldn't de-assert reset #%d\n", r);
+			// XXXNH cleanup
+			return;
+		}
+	}
+#endif
 	sc->sc_jpd = of_compatible_lookup(phandle, compat_data)->data;
 
 	mutex_init(&sc->sc_lock, MUTEX_DEFAULT, IPL_VM);
