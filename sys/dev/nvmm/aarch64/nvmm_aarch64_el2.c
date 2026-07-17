aa6e93b0e0c07e3f3f0b95ff52b6bbee9f331538
Nick Hudson <skrll@netbsd.org>
2026-07-17 07:03:25 +0100

    debug

--- sys/dev/nvmm/aarch64/nvmm_aarch64_el2.c
+++ sys/dev/nvmm/aarch64/nvmm_aarch64_el2.c
@@ -783,6 +783,13 @@ aarch64_el2_vmexit_irq(struct trapframe *tf)
                 reg_cntv_ctl_el0_write(cntv_ctl | CNTCTL_IMASK);
 	}
 
+	/* Debug */
+	uint32_t *hpa = (uint32_t *)aarch64_gva_to_pa(tf->tf_spsr, tf->tf_pc);
+	if (hpa != (void *)-1) {
+		struct nvmm_aarch64_exit *exit = &cpudata->exit;
+		exit->insn = le32toh(*hpa);
+	}
+
 	aarch64_vmexit_context(tf, cpudata);
 }
 
@@ -977,10 +984,12 @@ aarch64_el2_vmexit_trap(struct trapframe *tf)
 		break;
 	}
 
-// XXXNH used by the MMIO emulation code. hmm.
 	if (eclass != ESR_EC_INSN_ABT_EL_LOW &&
 	    eclass != ESR_EC_INSN_ABT_EL_CUR) {
-		// XXXNH defer to userland
+		/*
+		 * XXXNH this is used by the MMIO emulation code - defer to
+		 * userland?
+		 */
 		/*
 		 * read an instruction from PC.
 		 * if instruction abort, it cannot be read.
@@ -990,6 +999,12 @@ aarch64_el2_vmexit_trap(struct trapframe *tf)
 			exit->insn = e2h_enabled ?
 			    le32toh(*(uint32_t *)AARCH64_PA_TO_KVA(pa)) :
 			    le32toh(*(uint32_t *)pa);
+	}
+
+	/* Debug */
+	uint32_t *hpa = (uint32_t *)aarch64_gva_to_pa(tf->tf_spsr, tf->tf_pc);
+	if (hpa != (void *)-1) {
+		exit->insn = le32toh(*hpa);
 	}
 
 	// E2H needs to do eret to guest if not doing vmexit
